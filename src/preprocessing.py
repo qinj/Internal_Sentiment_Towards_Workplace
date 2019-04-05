119f109f3b501e0de36c017d87d59a5875c2d6e2
Jason Qin <qinj@umich.edu>
2019-04-05 11:08:26 -0700

    updated pickles location and preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,36 @@
-def create_clean_glassdoor_reviews_csv():
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import string
+import unicodedata
+
+from scipy import sparse
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.util import ngrams
+from nltk import pos_tag
+from nltk import RegexpParser
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+
+import pickle
+
+
+################################################################################
+#                                                                              #
+#                                CLEANING                                      #
+#                                                                              #
+################################################################################
+
+def preprocessing_clean_glassdoor():
     glassdoor_df = pd.read_csv('../data/google-amazon-facebook-employee-reviews/employee_reviews.csv', index_col=0)
     glassdoor_df.drop(['link'], axis=1, inplace=True)
     employee_titles_df = glassdoor_df["job-title"].str.split(" - ", n = 1, expand = True)
@@ -60,7 +92,7 @@ def create_clean_glassdoor_reviews_csv():
     glassdoor_df.to_csv('../data/clean_glassdoor_reviews.csv')
     return
 
-def create_amazon_reviews_csv(glassdoor_filepath, amazon_earnings_filepath):
+def preprocessing_create_amazon_with_earnings(glassdoor_filepath, amazon_earnings_filepath):
     glassdoor_df = pd.read_csv(glassdoor_filepath)
     q_earnings_df = pd.read_csv(amazon_earnings_filepath)
     amazon_reviews_df = glassdoor_df[glassdoor_df['company'] == 'amazon']
@@ -98,3 +130,89 @@ def create_amazon_reviews_csv(glassdoor_filepath, amazon_earnings_filepath):
         if row['senior-management-stars'] != row['senior-management-stars']:
             amazon_reviews_df.loc[i, 'senior-management-stars'] = senior_mean
     amazon_reviews_df.to_csv('../data/clean_amazon_reviews.csv')
+
+
+
+################################################################################
+#                                                                              #
+#                                   NLP                                        #
+#                                                                              #
+################################################################################
+def filter_tokens(sent):
+    stopwords_ = set(stopwords.words('english'))
+    punctuation_ = set(string.punctuation)
+
+    return([w for w in sent if not w in stopwords_ and not w in punctuation_])
+
+def validate_string(s):
+    letter_flag = False
+    number_flag = False
+    for i in s:
+        if i.isalpha():
+            letter_flag = True
+        if i.isdigit():
+            number_flag = True
+    return letter_flag and number_flag
+
+def extract_bow_from_column(column):
+    numeric = '0123456789.'
+    stopwords_ = set(stopwords.words('english'))
+    punctuation_ = set(string.punctuation)
+    special_char = ['%', '-', '/']
+    filtered_column = []
+    for input_string in column:
+        tokens_list = [sent for sent in map(word_tokenize, sent_tokenize(str(input_string)))]
+        tokens_filtered = list(map(filter_tokens, tokens_list))
+        # filter it more
+        for tokens in tokens_filtered:
+            for idx, token in enumerate(tokens):
+                if token.isdigit():
+                    tokens.pop(idx)
+                for char in special_char:
+                    if char in token:
+                        tokens.pop(idx)
+                        tokens.extend(token.split(char))
+        for tokens in tokens_filtered:
+            for idx, token in enumerate(tokens):
+                if validate_string(token):
+        #             both number and letter in it
+                    for i, c in enumerate(token):
+                        if c not in numeric:
+                            break
+                    number = token[:i]
+                    unit = token[i:]
+                    tokens.pop(idx)
+                    tokens.append(unit)
+        # either porter or snowball work for stemming
+        stemmer_porter = PorterStemmer()
+        tokens_stemporter = [list(map(stemmer_porter.stem, sent)) for sent in tokens_filtered]
+        if len(tokens_stemporter) == 0:
+            filtered_column.append('')
+        else:
+            filtered_column.append(' '.join(tokens_stemporter[0]))
+    return filtered_column
+
+def preprocessing_nlp(filepath):
+    """ filepath = ../data/clean_amazon_reviews.csv
+        creates the X and y csv files: df_with_nlp.csv and work-balance-stars.csv
+    """
+    amazon_df = pd.read_csv(filepath)
+    amazon_df['pros'] = amazon_df['pros'].str.lower()
+    amazon_df['filtered_pros'] = extract_bow_from_column(amazon_df['pros'])
+    corpus_pros = [row for row in amazon_df['filtered_pros']]
+    cv = CountVectorizer(max_features=2000)
+    cv_array = cv.fit_transform(corpus_pros).toarray()
+    cv_dict = {}
+    i = 0
+    for key in cv.vocabulary_:
+        cv_dict["word_" + key] = cv_array[:,i]
+        i += 1
+    cv_df = pd.DataFrame(cv_dict)
+    non_nlp_df = amazon_df[['culture-values-stars', 'career-opportunities-stars',
+                       'comp-benefit-stars', 'senior-management-stars', 'helpful-count',
+                       'is_current_employee', 'year', 'quarter', 'amazon_earnings_this_quarter']]
+
+    new_df = pd.concat([non_nlp_df, cv_df], axis=1)
+    new_df.to_csv("../data/df_with_nlp.csv")
+    amazon_df['work-balance-stars'].to_csv("../data/work-balance-stars.csv")
+    return
